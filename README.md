9ea8df23da0dde89ef13b88e8ccf2fbba8cb205c
Seedgou <i@zczc.cz>
2023-10-23 17:42:21 +0800

    init project

---
+++ README.md
@@ -0,0 +1,19 @@
+# 📄 IP InBrowser.App
+
+https://ip.inbrowser.app
+
+IP InBrowser.App is a tool to check IP. Fully runs in your browser. No server-side code.
+
+## ✨ Features
+
+* Everything is processed in your browser. No privacy risk.
+* Works without network connection using PWA.
+* Works on all modern browsers and devices.
+* All files are static. No backend servers needed.
+
+## 🧑‍💻 Develop
+
+```sh
+$ npm run dev # Run Dev Server
+$ npm run build # Build to dist/
+```
---
+++ src/views/HomeView.vue
@@ -0,0 +1,27 @@
+<template>
+  <main>
+    <n-h2 prefix="bar" align-text>Tools</n-h2>
+  </main>
+</template>
+
+<script setup lang="ts">
+import { NH2 } from "naive-ui";
+import { useHead } from "@vueuse/head";
+
+useHead({
+  title: "IP InBrowser.App",
+  meta: [
+    {
+      name: "description",
+      content:
+        "IP InBrowser.App is a tool to check IP. Fully runs in your browser. No server-side code.",
+    },
+  ],
+  link: [
+    {
+      rel: "canonical",
+      href: "https://ip.inbrowser.app/",
+    },
+  ],
+});
+</script>
