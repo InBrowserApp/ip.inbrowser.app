1649468a4f6095d09fb5def8f2a4b2d2e7469f37
Seedgou <i@zczc.cz>
2023-10-23 17:48:35 +0800

    my ip view

--- src/views/HomeView.vue
+++ src/views/HomeView.vue
@@ -1,11 +1,22 @@
 <template>
   <main>
     <n-h2 prefix="bar" align-text>Tools</n-h2>
+    <n-space vertical>
+      <router-link to="/tools/my-ip" #="{ navigate, href }" custom>
+        <n-button tag="a" :href="href" @click="navigate" text>
+          <template #icon>
+            <n-icon :component="DocumentSearch16Regular" />
+          </template>
+          My IP Address
+        </n-button>
+      </router-link>
+    </n-space>
   </main>
 </template>
 
 <script setup lang="ts">
-import { NH2 } from "naive-ui";
+import { NH2, NButton, NIcon, NSpace } from "naive-ui";
+import { DocumentSearch16Regular } from "@vicons/fluent";
 import { useHead } from "@vueuse/head";
 
 useHead({
---
+++ src/views/MyIPView.vue
@@ -0,0 +1,28 @@
+<template>
+  <main>
+    <BackToHome />
+    <n-h2 prefix="bar" align-text>My IP Address</n-h2>
+  </main>
+</template>
+
+<script setup lang="ts">
+import { NH2 } from "naive-ui";
+import { useHead } from "@vueuse/head";
+import BackToHome from "@/components/misc/BackToHome.vue";
+
+useHead({
+  title: "My IP Address | IP InBrowser.App",
+  meta: [
+    {
+      name: "description",
+      content: "Find my IP address. Fully runs in your browser. No server-side code.",
+    },
+  ],
+  link: [
+    {
+      rel: "canonical",
+      href: "https://ip.inbrowser.app/tools/my-ip",
+    },
+  ],
+});
+</script>
