8655fa11058dd67973de11cb044e2b1ddea90ef9
Seedgou <i@zczc.cz>
2023-10-23 18:37:23 +0800

    update my ip view layout

--- src/components/tools/my-ip/MyIP.vue
+++ src/components/tools/my-ip/MyIP.vue
@@ -1,12 +1,19 @@
 <template>
   <div>
-    IPv4: <IPDisplay :ip="ipv4" />
-    <br />
-    IPv6: <IPDisplay :ip="ipv6" />
+    <n-h3 prefix="bar" align-text>IPv4</n-h3>
+    <n-p>
+      <IPDisplay :ip="ipv4" />
+    </n-p>
+
+    <n-h3 prefix="bar" align-text>IPv6</n-h3>
+    <n-p>
+      <IPDisplay :ip="ipv6" />
+    </n-p>
   </div>
 </template>
 
 <script setup lang="ts">
+import { NH3, NP } from "naive-ui";
 import { getMyIPv4, getMyIPv6 } from "@/utils/ip/get-my-ip";
 import { computedAsync } from "@vueuse/core";
 import IPDisplay from "./IPDisplay.vue";
