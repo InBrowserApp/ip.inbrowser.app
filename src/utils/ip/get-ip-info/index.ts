f082214e70fb874c6981e9a64015bfc7251cf3d4
Seedgou <i@zczc.cz>
2023-10-24 15:24:12 +0800

    basic ip info

---
+++ src/utils/ip/get-ip-info/index.ts
@@ -0,0 +1,16 @@
+import IPSB from "./ip.sb";
+import type { IPInfo } from "./types.ts";
+
+export type { IPInfo } from "./types.ts";
+
+type IPInfoProviderFunction = (
+  ip: string,
+  options?: RequestInit
+) => Promise<IPInfo>;
+
+export type IPInfoProvider = {
+  name: string;
+  getIPInfo: IPInfoProviderFunction;
+};
+
+export const IPInfoProviders: IPInfoProvider[] = [IPSB];
