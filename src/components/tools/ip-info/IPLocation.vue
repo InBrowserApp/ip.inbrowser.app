83f93ba9ca73a3b9861841d2c57ce596de7adfe0
Seedgou <i@zczc.cz>
2023-10-24 16:17:42 +0800

    openlayers 3

---
+++ src/components/tools/ip-info/IPLocation.vue
@@ -0,0 +1,40 @@
+<template>
+  <ol-map
+    :loadTilesWhileAnimating="true"
+    :loadTilesWhileInteracting="true"
+    style="height: 400px"
+  >
+    <ol-view
+      ref="view"
+      :center="center"
+      :rotation="rotation"
+      :zoom="zoom"
+      :projection="projection"
+    />
+
+    <ol-tile-layer>
+      <OlSourceOSM />
+    </ol-tile-layer>
+
+    
+  </ol-map>
+</template>
+
+<script setup lang="ts">
+import { ref } from "vue";
+import { Map, Layers, Sources } from "vue3-openlayers";
+
+const OlMap = Map.OlMap;
+const OlView = Map.OlView;
+const OlTileLayer = Layers.OlTileLayer;
+const OlSourceOSM = Sources.OlSourceOSM;
+
+const props = defineProps<{
+  location: [number, number];
+}>();
+
+const center = ref(props.location);
+const projection = ref("EPSG:4326");
+const zoom = ref(8);
+const rotation = ref(0);
+</script>
