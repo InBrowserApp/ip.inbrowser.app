97354063dfa83320712f317084cc4dfe2b0ee80e
Seedgou <i@zczc.cz>
2023-10-25 21:52:19 +0800

    add catch-all route

--- src/router/index.ts
+++ src/router/index.ts
@@ -51,6 +51,12 @@ const router = createRouter({
       name: "ip-cidr-normalize",
       component: IPCIDRNormalizeView,
     },
+
+    // catch all
+    {
+      path: "/:pathMatch(.*)*",
+      redirect: { name: "home" },
+    },
   ],
 });
 
