96f03ec91db8607262df99e556bfe577ce1daca4
Seedgou <i@zczc.cz>
2023-10-26 23:07:09 +0800

    add description to my ip view

--- src/views/MyIPView.vue
+++ src/views/MyIPView.vue
@@ -2,12 +2,18 @@
   <main>
     <BackToHome />
     <n-h2 prefix="bar" align-text>My IP Address</n-h2>
+    <n-p>
+      This tool instantly displays your current IP address along with additional
+      information such as location and ISP, aiding in network configuration and
+      troubleshooting. Simply open the tool and your IP information is
+      immediately presented.
+    </n-p>
     <MyIP />
   </main>
 </template>
 
 <script setup lang="ts">
-import { NH2 } from "naive-ui";
+import { NH2, NP } from "naive-ui";
 import { useHead } from "@vueuse/head";
 import BackToHome from "@/components/misc/BackToHome.vue";
 import MyIP from "@/components/tools/my-ip/MyIP.vue";
@@ -18,7 +24,7 @@ useHead({
     {
       name: "description",
       content:
-        "Find my IP address. Fully runs in your browser. No server-side code.",
+        "This tool instantly displays your current IP address along with additional information such as location and ISP, aiding in network configuration and troubleshooting. Simply open the tool and your IP information is immediately presented.",
     },
   ],
   link: [
