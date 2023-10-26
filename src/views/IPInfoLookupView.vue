682a147394634384dd8bfdeb95923d22c3badaca
Seedgou <i@zczc.cz>
2023-10-26 23:04:43 +0800

    add description for ip info lookup view

--- src/views/IPInfoLookupView.vue
+++ src/views/IPInfoLookupView.vue
@@ -2,12 +2,18 @@
   <main>
     <BackToHome />
     <n-h2 prefix="bar" align-text>IP Info Lookup</n-h2>
+    <n-p>
+      This tool retrieves detailed information about an IP address, aiding in
+      network analysis and troubleshooting. Enter the IP, and get instant
+      information including location, ISP, and associated domain name.
+    </n-p>
+    <n-h3 prefix="bar" align-text>IPv4 / IPv6 / Domain</n-h3>
     <IPInfoSearchInput />
   </main>
 </template>
 
 <script setup lang="ts">
-import { NH2 } from "naive-ui";
+import { NH2, NP, NH3 } from "naive-ui";
 import { useHead } from "@vueuse/head";
 import BackToHome from "@/components/misc/BackToHome.vue";
 import IPInfoSearchInput from "@/components/tools/ip-info/IPInfoSearchInput.vue";
