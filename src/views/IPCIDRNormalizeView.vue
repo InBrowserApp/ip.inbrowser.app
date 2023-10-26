7b03ce7bb329dba8322885b6c1b69f654b61fe49
Seedgou <i@zczc.cz>
2023-10-26 22:54:12 +0800

    add description to IP CIDR Normalize

--- src/views/IPCIDRNormalizeView.vue
+++ src/views/IPCIDRNormalizeView.vue
@@ -1,23 +1,29 @@
 <template>
   <main>
     <BackToHome />
-    <n-h2 prefix="bar" align-text>IP/CIDR Normalize</n-h2>
+    <n-h2 prefix="bar" align-text>IP/CIDR Normalizer</n-h2>
+    <n-p
+      >This tool normalizes IP addresses and CIDR notation, supporting both IPv4
+      and IPv6 formats. Enter the IP or CIDR, and get a standardized format
+      instantly.</n-p
+    >
     <IPCIDRNormalize />
   </main>
 </template>
 
 <script setup lang="ts">
-import { NH2 } from "naive-ui";
+import { NH2, NP } from "naive-ui";
 import { useHead } from "@vueuse/head";
 import BackToHome from "@/components/misc/BackToHome.vue";
 import IPCIDRNormalize from "@/components/tools/ip-cidr-normalize/IPCIDRNormalize.vue";
 
 useHead({
-  title: "IP/CIDR Normalize | IP InBrowser.App",
+  title: "IP/CIDR Normalizer | IP InBrowser.App",
   meta: [
     {
       name: "description",
-      content: "IP/CIDR Normalize. IPv4 & IPv6 supported.",
+      content:
+        "This tool normalizes IP addresses and CIDR notation, supporting both IPv4 and IPv6 formats. Enter the IP or CIDR, and get a standardized format instantly.",
     },
   ],
   link: [
